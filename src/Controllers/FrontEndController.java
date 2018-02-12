56f64d9c82f75e53bab4b367cb53aa8bbd43b91d
Houssem Nouira <houssem.nouira@esprit.tn>
2018-02-12 22:52:40 +0100

    front end gui edited

--- src/Controllers/FrontEndController.java
+++ src/Controllers/FrontEndController.java
@@ -7,7 +7,13 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
 
 /**
  * FXML Controller class
@@ -16,12 +22,14 @@ import javafx.fxml.Initializable;
  */
 public class FrontEndController implements Initializable {
 
+	@FXML
+	private Label accueil;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+		
     }    
     
 }
