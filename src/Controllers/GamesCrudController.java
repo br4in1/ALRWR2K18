727dbd9776a635758bc774555d082442f6c91b40
S1mo <selim.maatoug@esprit.tn>
2018-02-13 21:33:16 +0100

    aezr

--- src/Controllers/GamesCrudController.java
+++ src/Controllers/GamesCrudController.java
@@ -6,24 +6,30 @@
 package Controllers;
 
 import Entities.Game;
+
 import Services.GameCrud;
-import com.jfoenix.controls.JFXTreeTableView;
+import Services.StadiumCrud;
+import Services.TeamCrud;
 import java.net.URL;
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import static javafx.scene.input.KeyCode.S;
+import static javafx.scene.input.KeyCode.T;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -33,7 +39,9 @@ import javafx.stage.Stage;
  *
  * @author simo
  */
+
 public class GamesCrudController implements Initializable {
+	
 
 	@FXML
 	private Pane pane;
@@ -59,14 +67,18 @@ public class GamesCrudController implements Initializable {
 	private TableColumn<Game, String> referee;
 	@FXML
 	private TableColumn<Game, Integer> id;
+	private HashMap<String, Integer> map1;
+	private HashMap<String, Integer> map2;
 
+	
 	/**
 	 * Initializes the controller class.
 	 */
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
+	
+	public void display()
+	{
 		id.setCellValueFactory(
-            new PropertyValueFactory<>("id"));
+            new  PropertyValueFactory<>("id"));
 		date.setCellValueFactory(
             new PropertyValueFactory<>("date"));
 		home.setCellValueFactory(
@@ -88,6 +100,53 @@ public class GamesCrudController implements Initializable {
 		ObservableList<Game> OL = FXCollections.observableList(GameCrud.findAllGames());
 		System.out.println(OL);
 		tablev.setItems(OL);
+	}
+	
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		display();
+		//update();
+		
+		map1 = TeamCrud.GetNameIdMap();
+		map2 = StadiumCrud.GetNameIdMap();
+		ObservableList<String> teams = FXCollections.observableArrayList(map1.keySet());
+		ObservableList<String> stadiums  = FXCollections.observableArrayList(map2.keySet());
+		
+		home.setCellFactory(ComboBoxTableCell.forTableColumn(teams));
+		away.setCellFactory(ComboBoxTableCell.forTableColumn(teams));
+		result.setCellFactory(TextFieldTableCell.<Game>forTableColumn());
+		stadium.setCellFactory(ComboBoxTableCell.forTableColumn(stadiums));
+		summary.setCellFactory(TextFieldTableCell.<Game>forTableColumn());
+		summaryPhoto.setCellFactory(TextFieldTableCell.<Game>forTableColumn());
+		highlights.setCellFactory(TextFieldTableCell.<Game>forTableColumn());
+		referee.setCellFactory(TextFieldTableCell.<Game>forTableColumn());
+		// ------ //
+		summary.setOnEditCommit(new EventHandler<CellEditEvent<Game,String>>() {
+            @Override
+            public void handle(CellEditEvent<Game, String> t) {
+                ((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).setSummary(t.getNewValue());
+				GameCrud.update("Summary", t.getNewValue(),((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).getId());
+            }
+        });
+		summaryPhoto.setOnEditCommit(new EventHandler<CellEditEvent<Game,String>>() {
+            @Override
+            public void handle(CellEditEvent<Game, String> t) {
+                ((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).setSummary(t.getNewValue());
+				GameCrud.update("summaryPhoto", t.getNewValue(),((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).getId());
+            }
+        });
+		away.setOnEditCommit(new EventHandler<CellEditEvent<Game,String>>() {
+            @Override
+            public void handle(CellEditEvent<Game, String> t) {
+                ((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).setAwayTeam(t.getNewValue());
+				GameCrud.update("AwayTeam", map1.get(t.getNewValue()),((Game) t.getTableView().getItems().get(t.getTablePosition().getRow())).getId());
+            }
+        });
+		
+		// ------ //
+		
 	}	
 
 	@FXML
@@ -103,6 +162,25 @@ public class GamesCrudController implements Initializable {
        e.printStackTrace();
 	}
     }
-	
+
+	@FXML
+	private void refresh(MouseEvent event) {
+		display();
+	}
+
+	@FXML
+	private void date(TableColumn.CellEditEvent<Game, Date> event) {
+			  try {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/Datepicker.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));  
+            stage.show();
+            
+    } catch(Exception e) {
+       e.printStackTrace();
+	}
+	}
+
 }
 
--- src/Services/GameCrud.java
+++ src/Services/GameCrud.java
@@ -12,6 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import Entities.Game;
 import Utils.DataSource;
+import com.sun.rowset.internal.Row;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -66,9 +67,41 @@ public class GameCrud {
 			System.out.println(result);
 			return result;
 		} catch (SQLException ex) {
-			Logger.getLogger(UserCrud.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger(GameCrud.class.getName()).log(Level.SEVERE, null, ex);
 		}
 		return null;
+	}
+		public static void update(String row,String value, int id) {
+		Connection con = DataSource.getInstance().getCon();
+		String strSQLQuery=String.format("update game set %s = '%s' where id=%s", row, value, id);
+			System.out.println(strSQLQuery);
+		try {
+			PreparedStatement ste = con.prepareStatement(strSQLQuery);
+			ste.executeUpdate();
+			
+
+			
+
+		
+		} catch (SQLException ex) {
+			Logger.getLogger(GameCrud.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+		public static void update(String row,int value, int id) {
+		Connection con = DataSource.getInstance().getCon();
+		String strSQLQuery=String.format("update game set %s = '%s' where id=%s", row, value, id);
+			System.out.println(strSQLQuery);
+		try {
+			PreparedStatement ste = con.prepareStatement(strSQLQuery);
+			ste.executeUpdate();
+			
+
+			
+
+		
+		} catch (SQLException ex) {
+			Logger.getLogger(GameCrud.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 }
