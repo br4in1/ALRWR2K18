faaeaa223fff702d5fb4ad3779fa5585a4039fd7
S1mo <selim.maatoug@esprit.tn>
2018-02-27 23:28:19 +0100

    Games added in front

--- src/Controllers/GamesController.java
+++ src/Controllers/GamesController.java
@@ -9,13 +9,20 @@ import Entities.Game;
 import Services.GameCrud;
 import Services.TeamCrud;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
@@ -29,19 +36,21 @@ public class GamesController implements Initializable {
 	@FXML
 	private GridPane groupA;
 	@FXML
-	private GridPane groupA1;
+	private GridPane groupB;
 	@FXML
-	private GridPane groupA2;
+	private GridPane groupC;
 	@FXML
-	private GridPane groupA3;
+	private GridPane groupD;
 	@FXML
-	private GridPane groupA4;
+	private GridPane groupE;
 	@FXML
-	private GridPane groupA5;
+	private GridPane groupF;
 	@FXML
-	private GridPane groupA6;
+	private GridPane groupG;
 	@FXML
-	private GridPane groupA7;
+	private GridPane groupH;
+	int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0;
+	int a1 = 0, b1 = 0, c1 = 0, d1 = 0, e1 = 0, f1 = 0, g1 = 0;
 
 	/**
 	 * Initializes the controller class.
@@ -51,10 +60,65 @@ public class GamesController implements Initializable {
 		List<Game> gamesList = GameCrud.findAllGames();
 		
 		for (Game game : gamesList) {
-			System.out.println(TeamCrud.findByName(game.getHomeTeam()).getWcGroup());
+			switch (TeamCrud.findByName(game.getHomeTeam()).getWcGroup()) {
+				case "B":
 				
-			
+					
+					if (b > 1) {
+						b = 0;
+						b1++;
+					}
+					groupB.add(match(TeamCrud.findByName(game.getHomeTeam()).getFlagPhoto(), TeamCrud.findByName(game.getAwayTeam()).getFlagPhoto(), game.getResult(), game.getStadium(), groupB.getChildren().size() + 1, game.getDate()), b, b1);
+					b++;
+					groupB.getChildren().get(groupB.getChildren().size() - 1).setStyle("-fx-padding: 10 20 0 10;");
+
+			}
+
 		}
-	}	
-	
+	}
+
+	public VBox match(String homeTeam, String AwayTeam, String result, String stadium, int i, Date d) {
+		VBox match = new VBox();
+		Pane title = new Pane();
+		title.setStyle("-fx-background-image: url(\"/assets/red.png\");");
+		title.setMinHeight(40);
+		Pane content = new Pane();
+		content.setStyle("-fx-background-color: #fcfcfc;");
+		content.setMinHeight(140);
+		match.getChildren().add(title);
+		match.getChildren().add(content);
+		HBox sd = new HBox();
+		HBox teams = new HBox();
+		ImageView homeT = new ImageView(homeTeam);
+		homeT.setFitHeight(53);
+		homeT.setFitWidth(95);
+		Label res = new Label(result);
+		res.setStyle("-fx-padding: 20 20 0 20;");
+		ImageView awayT = new ImageView(AwayTeam);
+		awayT.setFitHeight(53);
+		awayT.setFitWidth(95);
+		teams.getChildren().add(homeT);
+		teams.getChildren().add(res);
+		teams.getChildren().add(awayT);
+		teams.setSpacing(20);
+		teams.setStyle("-fx-padding: 45 0 0 80;");
+		content.getChildren().add(teams);
+		Label stadium1 = new Label(stadium);
+		Label gameNb = new Label("Game " + i);
+		Label date1 = new Label(String.valueOf(d));
+		sd.getChildren().add(stadium1);
+		sd.getChildren().add(gameNb);
+		sd.getChildren().add(date1);
+		stadium1.setTextFill(Color.WHITE);
+		stadium1.setPrefWidth(170);
+		gameNb.setTextFill(Color.WHITE);
+		gameNb.setStyle("-fx-padding: 10 100 0 30;");
+		stadium1.setStyle("-fx-padding: 10 -20 0 20;");
+		date1.setTextFill(Color.WHITE);
+		date1.setStyle("-fx-padding: 10 20 0 0;");
+		title.getChildren().add(sd);
+
+		return match;
+	}
+
 }
